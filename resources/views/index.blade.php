e3b30774b7a6547d3c4b38ae858124bf01942cd3
Heri Setiawan <madeheri14@gmail.com>
2023-10-05 14:25:02 +0700

    version 1.0.1

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<link rel="stylesheet" href="{{ url('/assets/style.css') }}">
+<div class="page-1">
+    <div class="rectangle-13"></div>
+
+    <div class="habib-adiba">Habib &amp; Adiba</div>
+
+    <div class="nama-tamu">Nama Tamu</div>
+
+    <img class="img-wedding-1-1" src="{{ url('img/couple-2.webp') }}" />
+
+    <div class="the-wedding-of">THE WEDDING OF</div>
+
+    <div class="kepada-bapak-ibu-saudara-i">Kepada Bapak/Ibu/Saudara/i</div>
+
+    <div class="button">
+      <div class="rectangle-14"></div>
+
+      <img class="envelope-1" src="{{ url('img/icon-envelope-open-white.png') }}" />
+
+      <div class="buka-undangan">Buka Undangan</div>
+    </div>
+
+    <div class="rectangle-15"></div>
+
+    <div class="mini-bunga-tema-06-atas-1">
+        
+      {{-- <img class="mini-bunga-tema-06-atas-12" src="mini-bunga-tema-06-atas-12.png"/> --}}
+    </div>
+  </div>
